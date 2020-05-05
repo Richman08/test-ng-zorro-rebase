a80db0042173f1fbacdfaf5959b0e396b241d540
Oleg <oloktionov1985@gmail.com>
2020-05-05 16:15:08 +0300

    feat (dev): add constructor and OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   theme = true;
   isCollapsed = false;
   array = [1, 2, 3, 4];
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
   toggleCollapsed(): void {
     this.isCollapsed = !this.isCollapsed;
